acff363b0dbe6a265e5c865389c1078713bd50a0
alejandrothomas <alejandrothomas@gmail.com>
2016-06-30 08:23:10 -0300

    Se agregó la descripción a las noticias
    Y también se implementó picasso pa’ las fotos

--- app/src/main/java/ar/com/thomas/mydailynews/view/NewsFlow/FragmentNewsViewPager.java
+++ app/src/main/java/ar/com/thomas/mydailynews/view/NewsFlow/FragmentNewsViewPager.java
@@ -1,12 +1,16 @@
 package ar.com.thomas.mydailynews.view.NewsFlow;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import ar.com.thomas.mydailynews.R;
 import ar.com.thomas.mydailynews.model.News;
@@ -17,8 +21,8 @@ import ar.com.thomas.mydailynews.model.News;
 public class FragmentNewsViewPager extends Fragment {
 
     public static final String NEWS_TITLE = "newsTitle";
-    public static final String NEWS_SUBTITLE = "newsSubtitle";
-    public static final String RSS_SOURCE = "rssFeed";
+    public static final String NEWS_DESCRIPTION = "newsDescription";
+    public static final String NEWS_IMAGE_URL = "newsImageUrl";
 
 
 
@@ -30,13 +34,16 @@ public class FragmentNewsViewPager extends Fragment {
         Bundle bundle = getArguments();
 
         String newsTitle = bundle.getString(NEWS_TITLE);
-        String newsSubtitle = bundle.getString(NEWS_SUBTITLE);
-        String rssSource = bundle.getString(RSS_SOURCE);
+        String newsDescription = bundle.getString(NEWS_DESCRIPTION);
+        String newsImageUrl = bundle.getString(NEWS_IMAGE_URL);
 
         TextView textViewNewsTitle = (TextView)view.findViewById(R.id.fragmentNewsViewPager_TEXTVIEW_Title);
         TextView textViewNewsSubtitle = (TextView)view.findViewById(R.id.fragmentNewsViewPager_TEXTVIEW_Subtitle);
+        ImageView imageViewImageUrl = (ImageView)view.findViewById(R.id.fragmentNewsViewPager_IMAGEVIEW_ImageURL);
+
         textViewNewsTitle.setText(newsTitle);
-        textViewNewsSubtitle.setText(newsSubtitle);
+        textViewNewsSubtitle.setText(newsDescription);
+        Picasso.with(getActivity()).load(newsImageUrl).placeholder(R.drawable.unavailable_image).into(imageViewImageUrl);
 
         return view;
     }
@@ -48,6 +55,8 @@ public class FragmentNewsViewPager extends Fragment {
         Bundle arguments = new Bundle();
 
         arguments.putString(NEWS_TITLE, news.getTitle());
+        arguments.putString(NEWS_DESCRIPTION, news.getDescription());
+        arguments.putString(NEWS_IMAGE_URL, news.getImageUrl());
         fragmentNewsViewPager.setArguments(arguments);
         return fragmentNewsViewPager;
     }
